a4990d687b0370b797898e431523f52598180a15
David Rynearson <drynears@asu.edu>
2014-08-07 17:47:14 +0000

    Added in drupal and drush

--- bootstrap.sh
+++ bootstrap.sh
@@ -87,3 +87,25 @@ fi
 
 sudo rm -rf /var/www
 sudo ln -fs /vagrant /var/www
+
+## Installing Drupal (Maybe in the future I break this into a seperate script)
+cd /vagrant
+echo 'What would you like to call your new project'
+read PROJECT_NAME
+echo 'Project Name: $PROJECT_NAME accepted'
+echo 'Beginning Installation of Default Drupal installation and modules'
+
+wget http://ftp.drupal.org/files/projects/drupal-x.x.tar.gz
+tar -xzvf drupal-x.x.tar.gz
+# Cleanup
+rm drupal-7.3.tar.gz
+mv drupal-7.3/* $PROJECT_NAME
+sudo rm -r drupal-7-3
+
+# Drush Time
+sudo pear channel-discover pear.drush.org
+sudo pear install drush/drush
+
+cd $PROJECT_NAME
+
+
