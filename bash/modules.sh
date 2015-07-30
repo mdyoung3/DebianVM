595465b28b9470c3c0692d8b4db18e40e509ee58
mdyoung3 <mdyoung3@asu.edu>
2015-07-30 23:40:57 +0000

    This script will install a set of modules.

---
+++ bash/modules.sh
@@ -0,0 +1,75 @@
+#!bin/bash
+
+pause() {
+   read -r -p "$*" key
+}
+
+echo "Welcome to the fastrack module installation script. This is designed for the speedy installation of Drupal modules. 
+
+NOTE: THIS NEEDS TO BE RUN WITH BASH COMMAND, AND NOT THE SH COMMAND.
+
+Enter the name of the folder the files for the Drupal site."
+read folder
+
+if [ ! -d "/vagrant/$folder.dev" ]; then
+	echo "This folder does not exist"
+	echo "please try again."
+	exit
+fi
+
+#alias site_home="cd /vagrant/$folder.dev"
+#site_home
+#echo $site_home
+
+sitehome () {
+	cd /vagrant/$folder.dev
+}
+
+sitehome
+
+pause 'This script will give you the option to quickly install some commonly implemented modules and themes.
+
+Press Enter to continue.' 
+
+echo 'Do you want to install a theme? (yes/no)'
+read theme_ans
+
+if [ "$theme_ans" = 'yes' ]; then
+        echo "Choose a theme and type its number:
+	1. Boron
+	2. bootstrap"
+	read theme_name
+
+	if [ "$theme_name" = 1 ]; then
+		drush dl boron
+		drush en boron
+		drush vset theme_default boron
+	elif [ "$theme_name" = 2 ]; then
+		drush dl bootstrap
+		drush en bootstrap
+		drush vset theme_default boron
+	fi		
+        
+fi
+
+declare -a modules=( "date" "ctools" "date" "views" "honeypot" )
+echo 'You can install the following modules now:'
+printf '%s\n' "${modules[@]}"
+
+echo 'Would you like to install the above modules? (yes/no)'
+read module_ans
+
+if [ "$module_ans" = 'yes' ]; then
+	echo "begining installation process"
+
+	arraylength=${#modules[@]}	
+
+	for (( i=1; i<${arraylength}+1; i++ ));
+	do
+		drush dl ${modules[i-1]}
+		drush en ${modules[i-1]}
+	done
+fi 
+exit
+
+	
